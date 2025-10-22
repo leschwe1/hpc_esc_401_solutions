62ce974c5313dc845814d42e62ac03f59e4e4e86
Leschwe1 <leandro.schweiss@gmail.com>
2025-10-22 13:29:53 +0200

    bugfixes

--- exercise_session_05/poisson_solver/jacobi.cpp
+++ exercise_session_05/poisson_solver/jacobi.cpp
@@ -14,7 +14,7 @@ double norm_diff(params p, double** mat1, double** mat2){
 
     double sum = 0.0;
     for (int i = 0; i < p.nx ; i++){
-        for int j = 0, j < p.ny; j++{
+        for (int j = 0; j < p.ny; j++){
             double diff = mat1[i][j] - mat2[i][j];
             sum += diff * diff;
         }
