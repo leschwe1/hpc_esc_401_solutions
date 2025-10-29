d9654a154226a9e19b7b0a00f343ab1d971c6d6d
Leschwe1 <leandro.schweiss@gmail.com>
2025-10-29 15:05:40 +0100

    - implemented ring_2.c - answered question to ring_2.c in the md

---
+++ exercise_session_06/ring_and_deadlocks/ring_2.c
@@ -0,0 +1,40 @@
+#include "mpi.h"
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    // Initialize the MPI environment
+    MPI_Init(NULL, NULL);
+
+    // Get the number of processes and rank of the process
+    int size, my_rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+    int my_sum = 0;
+
+    int send_rank = my_rank;  // Send    buffer
+    int recv_rank = 0;        // Receive buffer
+    int my_sum = 0;     //initialize sum that is returned
+
+    // Compute the ranks of left/right neighbours 
+        //with the % size we ensure that the sizeNo is respected and the circle is closed
+    int left_rank = (my_rank - 1 + size) % size; 
+    int right_rank = (my_rank + 1) % size;
+
+    // Loop over the number of processes
+    //     send to right, receive from left
+    //     update the send buffer
+    //     update the local sum
+    for (int i = 0; i < size; i++) {
+        MPI_Irecv(&recv_rank, 1, MPI_INT, left_rank, 0, MPI_COMM_WORLD, &requests[0]);
+        MPI_Isend(&send_rank, 1, MPI_INT, right_rank, 0, MPI_COMM_WORLD, &requests[1]);
+        MPI_Waitall(2, requests, MPI_STATUSES_IGNORE);
+        my_sum += recv_rank;
+        send_rank = recv_rank;
+    }
+
+    printf("I am processor %d out of %d, and the sum is %d\n", my_rank, size, my_sum);
+
+    // Finalize the MPI environment.
+    MPI_Finalize();
+}
--- task1.md
+++ task1.md
@@ -1,6 +1,10 @@
-**What happens when you run the code? Why?**
+**ring.c: What happens when you run the code? Why?**
 So in this first version we have the following issue: all processes try to send simultaneously. But this is not possible as we would need to receive first (SSend blocks until received), before we can send.
 So no process is able to send, no process will receive so we are in a deadlock.
 
-**Run the code again. What changed and why did it solve the problem? Is it an optimal solution?**
+**ring_1.c:Run the code again. What changed and why did it solve the problem? Is it an optimal solution?**
 In this solution now we split the processes into two grops. one sends first, the other one receives first. Thsi breaks the deadlock we had befor as half of the processes are receiving while the others are sending. 
+
+
+**ring_2.c: Why are these methods better solutions than synchronous communication for our ring problem?**
+First of all we can see the code is much shorter and less complex. In addition to this, we do not need even/odd ordering anymore, with the now used methods, all processes can send and receive at the same time, but without ending in a deadlock. So in short it is solved more elegant
