6884bd799d90f5af5cd6e1bec37ce7044f914eee
leschwe1 <leandro.schweiss@uzh.ch>
2025-12-04 11:19:32 +0100

    task 3 done

--- exercise_session_11/nbody/nbody.cpp
+++ exercise_session_11/nbody/nbody.cpp
@@ -1,14 +1,14 @@
 #include <random>
 #include <vector>
+#include <cmath>
 
 using std::vector;
 
-struct particle {
-    float x, y, z;	// position
-    float vx, vy, vz;	// velocity
-    float ax, ay, az;	// acceleration
+struct particles {
+    vector<float> x, y, z;	// position
+    vector<float> vx, vy, vz;	// velocity
+    vector<float> ax, ay, az;	// acceleration
 };
-typedef vector<particle> particles;
 
 // Initial conditions
 void ic(particles &plist, int n) {
@@ -16,35 +16,54 @@ void ic(particles &plist, int n) {
     std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
     std::uniform_real_distribution<float> dis(0.0, 1.0);
 
-    plist.clear(); 	// Remove all particles
-    plist.reserve(n);	// Make room for "n" particle
-    for( auto i=0; i<n; ++i) {
-	particle p { dis(gen),dis(gen),dis(gen),0,0,0,0,0,0 };
-	plist.push_back(p);
-    }
+    // Resize all 9 vectors at once
+    plist.x.resize(n);
+    plist.y.resize(n);
+    plist.z.resize(n);
+    plist.vx.resize(n);
+    plist.vy.resize(n);
+    plist.vz.resize(n);
+    plist.ax.resize(n);
+    plist.ay.resize(n);
+    plist.az.resize(n);
 
+    for (int i = 0; i < n; ++i) {
+        plist.x[i] = dis(gen);
+        plist.y[i] = dis(gen);
+        plist.z[i] = dis(gen);
+        plist.vx[i] = 0.0f;
+        plist.vy[i] = 0.0f;
+        plist.vz[i] = 0.0f;
+        plist.ax[i] = 0.0f;
+        plist.ay[i] = 0.0f;
+        plist.az[i] = 0.0f;
+    }
 }
+
 
 void forces(particles &plist) {
-    int n = plist.size();
-    for(int i=0; i<n; ++i) { 		// We want to calculate the force on all particles
-	float ax=0, ay=0, az=0;
-        for(int j=0; j<n; ++j) {			// Depends on all other particles
-	    if (i==j) continue; // Skip self interaction 
-	    auto dx = plist[j].x - plist[i].x;
-	    auto dy = plist[j].y - plist[i].y;
-	    auto dz = plist[j].z - plist[i].z;
-	    auto r = sqrt(dx*dx + dy*dy + dz*dz);
-	    auto ir3 = 1 / (r*r*r);
-	    ax += dx * ir3;
-	    ay += dy * ir3;
-	    az += dz * ir3;
-	}
-	plist[i].ax = ax;
-	plist[i].ay = ay;
-	plist[i].az = az;
+    int n = plist.x.size();  // or plist.y.size() / plist.z.size()
+    for (int i = 0; i < n; ++i) {
+        float ax = 0.0f, ay = 0.0f, az = 0.0f;
+        for (int j = 0; j < n; ++j) {
+            if (i == j) continue;
+
+            float dx = plist.x[j] - plist.x[i];
+            float dy = plist.y[j] - plist.y[i];
+            float dz = plist.z[j] - plist.z[i];
+            float r = sqrtf(dx*dx + dy*dy + dz*dz);
+            float ir3 = 1.0f / (r*r*r);
+
+            ax += dx * ir3;
+            ay += dy * ir3;
+            az += dz * ir3;
+        }
+        plist.ax[i] = ax;
+        plist.ay[i] = ay;
+        plist.az[i] = az;
     }
 }
+
 
 int main(int argc, char *argv[]) {
     int N=50'000;
